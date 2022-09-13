27ee9a4124c41bcd123a6b637ac5d73cc6d7846e
skrajup <skrajprof1947@gmail.com>
2022-09-13 11:56:00 +0530

    update feedback url

--- uninstallation/uninst.js
+++ uninstallation/uninst.js
@@ -4,29 +4,35 @@ const quote = document.querySelector(".quote");
 form.addEventListener("submit", (e)=>{
     e.preventDefault();
     // console.log(form.feedback.value);
-    chrome.storage.sync.get("feedbacks").then((items)=>{
-        if(items && form.feedback.value.length > 0){
-            const feedbacks = items["feedbacks"];
-            if(feedbacks){
-                feedbacks.push({
-                    "date": new Date().toLocaleString(),
-                    "feedback": form.feedback.value
-                });
-                chrome.storage.sync.set({"feedbacks": feedbacks }).then(()=>{
-                    form.reset();
-                    form.style.display = "none";
-                    quote.style.display = "none";
-                    const h1 = document.createElement("h1");
-                    h1.textContent = "Thanks for letting us...";
-                    document.querySelector(".feedback").appendChild(h1);
-                }).catch(err=>{
-                    console.log(err);
-                });
-            }
-        }else{
-            console.log("empty field");
-        }
-    }).catch(err=>{
-        console.log(err);
-    });
+    // chrome.storage.sync.get("feedbacks").then((items)=>{
+    //     if(items && form.feedback.value.length > 0){
+    //         const feedbacks = items["feedbacks"];
+    //         if(feedbacks){
+    //             feedbacks.push({
+    //                 "date": new Date().toLocaleString(),
+    //                 "feedback": form.feedback.value
+    //             });
+    //             chrome.storage.sync.set({"feedbacks": feedbacks }).then(()=>{
+    //                 form.reset();
+    //                 form.style.display = "none";
+    //                 quote.style.display = "none";
+    //                 const h1 = document.createElement("h1");
+    //                 h1.textContent = "Thanks for letting us...";
+    //                 document.querySelector(".feedback").appendChild(h1);
+    //             }).catch(err=>{
+    //                 console.log(err);
+    //             });
+    //         }
+    //     }else{
+    //         console.log("empty field");
+    //     }
+    // }).catch(err=>{
+    //     console.log(err);
+    // });
+    form.reset();
+    form.style.display = "none";
+    quote.style.display = "none";
+    const h1 = document.createElement("h1");
+    h1.textContent = "Thanks for letting us...";
+    document.querySelector(".feedback").appendChild(h1);
 });
